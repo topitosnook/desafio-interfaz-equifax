8ab72fc8bd9091bb8045a72bd7daf53c3957e0c8
Valeria Argomedo <valeriaargomedo@gmail.com>
2021-10-20 12:42:00 -0300

    Generacion de carpetas,generacion de componentes, agregada la data de adaptadores

---
+++ src/components/Adapters.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Adapters() {
+  return (
+    <>
+      <div>{/* Guardados los adaptadores */}</div>
+      <div>{/* Informacion adaptador elegido - formulario */}</div>
+    </>
+  );
+}
